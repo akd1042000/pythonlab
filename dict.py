fceac676441151b0801b038a746982756871764b
akd1042000 <47100188+akd1042000@users.noreply.github.com>
2019-01-28 13:57:15 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,10 @@
+
+d={}
+x=int(input('number of student  '))
+for i in range(x):
+    n=input('enter the name ')
+    g=int(input('enter the gr no  '))
+    b=input('enter the branch  ')
+    d[g]=(n,b)
+print(d)
+
